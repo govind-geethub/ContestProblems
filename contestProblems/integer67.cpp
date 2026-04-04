0ba38bc4c433f4b07f4c859b1bd17487b58f65fa
govind-geethub <avin14a@gmail.com>
2026-04-04 23:22:21 +0530

    codeforces div4

---
+++ contestProblems/integer67.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int integer67(int x)
+{
+    return 67;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int x;
+        cin >> x;
+
+        cout << integer67(x) << endl;
+    }
+    return 0;
+}
